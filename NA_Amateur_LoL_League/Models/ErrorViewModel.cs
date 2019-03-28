a92ec01b10a98dc944c3defc41e3d60521d799fd
Ansem571 <jadams.macdonnell1@gmail.com>
2019-03-28 11:48:02 -0400

    Initial push

---
+++ NA_Amateur_LoL_League/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NA_Amateur_LoL_League.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
