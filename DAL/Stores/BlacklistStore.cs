20513e7fb6b5274588a65dec4aecaf257c17837d
Ansem571 <jadams.macdonnell1@gmail.com>
2019-05-14 15:27:23 -0400

    Blacklisted

---
+++ DAL/Stores/BlacklistStore.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DAL.Data.Implementations;
+using DAL.Databases.Interfaces;
+using DAL.Entities.UserData;
+
+namespace DAL.Stores
+{
+    public class BlacklistStore : TableStorageRepository<BlacklistEntity>
+    {
+        public BlacklistStore(IDatabase database) : base(database)
+        {
+        }
+    }
+}
---
+++ Domain/Repositories/Implementations/BlacklistRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL.Data.Interfaces;
+using DAL.Entities.UserData;
+using Domain.Repositories.Interfaces;
+
+namespace Domain.Repositories.Implementations
+{
+    public class BlacklistRepository : IBlacklistRepository
+    {
+        private readonly ITableStorageRepository<BlacklistEntity> _table;
+
+        public BlacklistRepository(ITableStorageRepository<BlacklistEntity> table)
+        {
+            _table = table ?? throw new ArgumentNullException(nameof(table));
+        }
+
+        public async Task<BlacklistEntity> GetByUserIdAsync(Guid userId)
+        {
+            var result = (await _table.ReadManyAsync("UserId = @userId", new {userId}, top: 1)).FirstOrDefault();
+            return result;
+        }
+    }
+}
