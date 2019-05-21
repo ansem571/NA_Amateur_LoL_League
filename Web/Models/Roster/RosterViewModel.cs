4d883da71a583cbda1f5093915de4e65cba70ac5
Matthew Ryan MacDonnell <jadams.macdonnell1@gmail.com>
2019-05-21 15:01:24 -0400

    updated views to include links

--- Web/Models/Roster/RosterViewModel.cs
+++ Web/Models/Roster/RosterViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using Domain.Views;
+using Microsoft.AspNetCore.Mvc.ViewFeatures.Internal;
 
 namespace Web.Models.Roster
 {
@@ -9,5 +11,22 @@ namespace Web.Models.Roster
         public RosterView RosterView { get; set; }
         public string StatusMessage { get; set; }
         public IEnumerable<ScheduleView> ScheduleLineup { get; set; }
+        public string TeamOpGg => Setup();
+
+        private string Setup()
+        {
+            var str = "https://na.op.gg/summoner/userName=";
+
+            var urls = RosterView.Players.Select(x => x.OpGgUrl).ToList();
+            foreach (var url in urls)
+            {
+                var urlSecure = url.Replace("http:", "https:");
+                var urlName = urlSecure.Replace("https://na.op.gg/summoner/userName=", "");
+                str += $"{urlName},";
+            }
+
+            str = str.Substring(0, str.Length - 1);
+            return str;
+        }
     }
 }
